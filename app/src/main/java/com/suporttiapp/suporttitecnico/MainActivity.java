cbf33f52100ed412055cb55ce5b34db94d5db998
Jarquison <jacksonlira.rs@gmail.com>
2018-01-16 22:59:03 -0300

    método de logar e salvar o em preferências compartilhadas

--- app/src/main/java/com/suporttiapp/suporttitecnico/MainActivity.java
+++ app/src/main/java/com/suporttiapp/suporttitecnico/MainActivity.java
@@ -1,5 +1,6 @@
 package com.suporttiapp.suporttitecnico;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -7,10 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String NOME_PREFERENCE = "REFERENCIA_LOGIN_AUTOMATICO";
+    private static final String PREFER_NAME = "Ref";
+    private static final String SENHA_ACESSO = "senha";
 
     private EditText senha;
     private Button btLogin;
@@ -23,15 +25,39 @@ public class MainActivity extends AppCompatActivity {
 
         senha = findViewById(R.id.senha);
         btLogin = findViewById(R.id.login_enviar);
+
+        logar();
+        cadastrarTecnico();
+
+    }
+
+    private void logar() {
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences.Editor editor = getSharedPreferences(NOME_PREFERENCE, MODE_PRIVATE).edit();
-                editor.putString("login", senha.getText().toString());
-                editor.commit();
+                // requisição ao servidor, encontrar a chave e retorna o técnico.
+
+
+
+                SharedPreferences preferences = getSharedPreferences(PREFER_NAME, Context.MODE_PRIVATE);
+                String sSalva = preferences.getString(SENHA_ACESSO, "");
+                if (sSalva != null){
+                    if (sSalva.equals(senha.getText().toString())){
+                        // iniciar seção e ir para outra atividade
+                        Intent intent = new Intent(MainActivity.this, TelaInicial.class);
+                        startActivity(intent);
+                    }
+
+                }else {
+                    Toast toast = Toast.makeText(getApplicationContext(), R.string.ERR_SENHA_NAO_CADASTRADA, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+
             }
         });
+    }
 
+    public void cadastrarTecnico(){
         btActCadastro = findViewById(R.id.btActCadastro);
         btActCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +66,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 }
